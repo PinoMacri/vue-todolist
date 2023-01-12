c584082a4d1a69772d0616c1a0960bd75be0f38f
PinoMacri <pinonegozio1@outlook.it>
2023-01-12 02:34:56 +0100

    Esercizio Consegnato

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("vue" , Vue)
+const app = Vue.createApp({
+    data() {
+        return {
+            lists: [
+                {
+                    text: "Fare i compiti",
+                    done: null,
+                    executive: null,
+                },
+                {
+                    text: "Fare la spesa",
+                    done: null,
+                    executive: null,
+                },
+                {
+                    text: "Fare faccende domestiche",
+                    done: null,
+                    executive: null,
+                },
+            ],
+            dinamicList:" ",
+        }
+    },
+    methods: {
+        addTask(){
+          if(this.dinamicList){
+            const newTask = {
+              text: this.dinamicList,
+              done: null,
+              executive: null,
+            }
+            this.lists.push(newTask);
+            this.dinamicList = '';
+          }
+        },
+    }
+})
+app.mount("#padre")
